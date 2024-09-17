dd66144a85bfef013a8da3b00c08754f44f87682
ahmdarjn <ahmadarjun347@gmail.com>
2024-09-17 11:29:48 +0700

    34 ARRAY ASOSIATIF

--- algoritma4-main/34 Array Asosiatif.php
+++ algoritma4-main/34 Array Asosiatif.php
@@ -11,4 +11,11 @@ echo "<h2>".$artikel["judul"]."</h2>";
 echo "<p>oleh: ".$artikel["penulis"]."</p>";
 echo "<p>View: ".$artikel["view"]."</p>";
 
-echo "<p><strong><i>By : Ahhmadd arjunn trisula</strong>";
+echo "<p><strong><i>By : Ahhmadd arjunn trisula</strong><hr>";
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "dian@ahmadimuslim.com";
+$email["isi"] = "Apa kabar? sudah lama tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
