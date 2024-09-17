fc8bfd84636828dcb7fe30e3d2025680f94ce190
ahmdarjn <ahmadarjun347@gmail.com>
2024-09-17 11:37:31 +0700

    36 MENYERTAKAN NILAI KEY

--- algoritma4-main/36 Menyertakan nilai key.php
+++ algoritma4-main/36 Menyertakan nilai key.php
@@ -4,4 +4,8 @@ foreach ($bulan as $index => $nama_bulan) {
 	echo ($index + 1) . '. ' . $nama_bulan . '<br/>';
 }
 
-echo "<p><strong><i>By : Ahmad arjun trisula</strong>";
+echo "<p><strong><i>By : Ahmad arjun trisula</strong><hr>";
+$kendaraan = array('jenis' => 'Mobil', 'merk' => 'Toyota', 'tipe' => 'Vios');
+foreach ($kendaraan as $key => $val) {
+	echo ucfirst($key) . ': ' . $val . '<br/>';
+}
